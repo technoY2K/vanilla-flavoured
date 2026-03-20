51a0496d5ff8dd8494a85ee5fccc53c642d8a6c6
technoY2K <cloud@kevia.me>
2026-03-20 14:23:51 -0700

    chore: add rollup-plugin-visualizer for bundle analysis

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   build: {
     outDir: "dist",
   },
+  plugins: [
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
 });
